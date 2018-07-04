4dd29e1ea94bb644f8abaab09952be5174376562
yoonjong.lee <normandi66@naver.com>
2018-07-05 00:27:34 +0900

    added factorial codes

---
+++ rec_add.py
@@ -0,0 +1,10 @@
+
+def rec_factorial(n):
+    #print(n)
+	if n <= 1:
+	    return 1
+	else:
+	    return n + rec_factorial(n-1)
+
+num = input("input number: ")
+print(rec_factorial(int(num)))
---
+++ rec_func.py
@@ -0,0 +1,6 @@
+def hello():
+    print("hi")
+	hello()
+
+
+hello()
