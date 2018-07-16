518da59bfff8066a1f357da8cd507c7bd8164de3
yoonjong.lee <normandi66@naver.com>
2018-07-17 00:21:16 +0900

    added simple linear search

---
+++ search_lin.py
@@ -0,0 +1,15 @@
+def num_search(list, num):
+	n = len(list)
+	num = int(num)
+	for i in range(n):
+		#print(list[i])
+		#print(num)
+		if num == int(list[i]):
+			return i
+	
+	return -1
+		
+list = [4, 5, 6, 7, 78, 998, 12]
+print(num_search(list, 6))
+print(num_search(list, 78))
+print(num_search(list, 2))
