f66b28ab39220d5bb76cab44e7ff420b5e67cbbd
yoonjong.lee <normandi66@naver.com>
2018-07-25 06:55:06 +0900

    added normal sort program

---
+++ ssort.py
@@ -0,0 +1,18 @@
+def sel_sort(d):
+    for i in range(len(d)-1):
+        print(i)
+        #print(d[i])
+        for j in range(i+1, len(d)):
+            print(j)
+            #print(d[j])
+            #if (d[i] > d[j]): # sort by min order
+            if (d[i] < d[j]): # sort by max order
+                temp = d[j]
+                d[j] = d[i]
+                d[i] = temp
+
+#d = [9, 3, 1, 2, 5]
+d = [2, 4, 5, 1, 3]
+#print(str(len(d)))
+sel_sort(d)
+print(d)
