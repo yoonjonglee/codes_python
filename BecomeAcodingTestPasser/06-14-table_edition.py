6344eee106774dd8e520d51d1e11d6e9f1790ed5
yoonjong.lee <yoonjong.lee@lge.com>
2025-10-16 13:54:46 +0900

    updated comments and output format to string

--- BecomeAcodingTestPasser/06-14-table_edition.py
+++ BecomeAcodingTestPasser/06-14-table_edition.py
@@ -11,42 +11,42 @@ sol = -1
 
 # code here
 def solve(n, k, cmd):
-    table  = ["O"] * n
-    deleted = []
-    cur = k
+    table  = ["O"] * n # O: not deleted, X: deleted
+    deleted = [] # stack of deleted row indices
+    cur = k # current cursor position
     for c in cmd:
-        if c[0] == "D":
-            x = int(c.split()[1])
+        if c[0] == "D": # down
+            x = int(c.split()[1]) # number of moves
             for _ in range(x):
-                cur += 1
+                cur += 1 # move down
                 while cur < n and table[cur] == "X":
-                    cur += 1
-        elif c[0] == "U":
-            x = int(c.split()[1])
+                    cur += 1 # skip deleted rows
+        elif c[0] == "U": # up
+            x = int(c.split()[1]) # number of moves
             for _ in range(x):
-                cur -= 1
+                cur -= 1 #
                 while cur >= 0 and table[cur] == "X":
-                    cur -= 1
-        elif c[0] == "C":
-            table[cur] = "X"
-            deleted.append(cur)
+                    cur -= 1 # skip deleted rows
+        elif c[0] == "C": # delete
+            table[cur] = "X" # mark as deleted
+            deleted.append(cur) # add to deleted stack
             # move cursor
-            next_cur = cur + 1
+            next_cur = cur + 1 # try to move down first
             while next_cur < n and table[next_cur] == "X":
-                next_cur += 1
-            if next_cur < n:
-                cur = next_cur
-            else:
-                prev_cur = cur - 1
+                next_cur += 1 # skip deleted rows
+            if next_cur < n: # can move down
+                cur = next_cur # move down  
+            else: # can't move down, move up
+                prev_cur = cur - 1 # try to move up
                 while prev_cur >= 0 and table[prev_cur] == "X":
-                    prev_cur -= 1
-                cur = prev_cur
-        elif c[0] == "Z":
-            if deleted:
-                recover = deleted.pop()
-                table[recover] = "O"
+                    prev_cur -= 1 # skip deleted rows
+                cur = prev_cur # move up
+        elif c[0] == "Z": # recover
+            if deleted: # if there is any deleted row
+                recover = deleted.pop() # get the last deleted row
+                table[recover] = "O" # mark as not deleted
 
-    return table
+    return "".join(table)
 
 sol = solve(n, k, cmd)
 
