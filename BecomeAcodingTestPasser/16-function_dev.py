814b8386acbaf988efb500970906a42df6127b12
yoonjong.lee <yoonjong.lee@lge.com>
2025-10-17 20:00:52 +0900

    added new quiz code - function development

---
+++ BecomeAcodingTestPasser/16-function_dev.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+
+inputs = sys.stdin.readline
+
+prog = list(map(int, inputs().split()))
+spd = list(map(int, inputs().split()))
+
+# code here
+
+
+# output
+#print(prog, spd)
