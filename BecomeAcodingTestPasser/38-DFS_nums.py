b53e4e7b60a477ea1bb713800424aaecd3b9efb4
yoonjong.lee <yoonjong.lee@lge.com>
2025-10-23 22:48:52 +0900

    updated practice of DFS

--- BecomeAcodingTestPasser/38-DFS_nums.py
+++ BecomeAcodingTestPasser/38-DFS_nums.py
@@ -1,22 +1,34 @@
 import sys
-import itertools
-from collections import deque
+from collections import defaultdict
+#import itertools
+#from collections import deque
 
 # input
-graph = [['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']] #[start, end]
+#graph = [['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']] #[start, end]
+graph = [['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']] 
 
 # code here
-def graph(graph, s):
-    v = set()
-    stack = []
-    stack.append(s)
-    while stack:
-        n = stack.pop()
-        if n not in v: v.add(n)
-        else: stack.append(
 
-s = graph
-sol = graph(graph, s)
+al = defaultdict(list)
+#v = set()
+v = []
+stack = []
+
+def DFS(graph, start):
+    for s, e in graph:
+        al[s].append(e)
+    stack=[start]
+    while stack:
+        #print(stack)
+        n = stack.pop()
+        if n not in v: 
+            #v.add(n)
+            v.append(n)
+            stack.extend(reversed(al[n]))
+    
+    return list(v)
+
+sol = DFS(graph, 'A')
 
 # output
 print(sol)
