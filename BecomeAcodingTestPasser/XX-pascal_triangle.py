dfe4df1048abcd6c4496519dccad5a9962611576
yoonjong.lee <yoonjong.lee@lge.com>
2025-11-12 15:14:51 +0900

    added the line of printing n'th row of the P's triangle

--- BecomeAcodingTestPasser/XX-pascal_triangle.py
+++ BecomeAcodingTestPasser/XX-pascal_triangle.py
@@ -3,29 +3,30 @@ import sys
 inputs = sys.stdin.readline
 
 #n = int(inputs())
-n = 8
+n = 4
 # code here
 t = [[1]] # first row
-
-for x in range(1, n): # build row x
+for x in range(1, n+1): # build row x
     pr = t[x-1] # previous row
-    nr = [1] # new row
+    nr = [1] # 1st value of the new row
     for y in range(len(pr)-1): # build middle values
         nr.append(pr[y]+pr[y+1]) # sum of two above
-    nr.append(1) # last value
+    nr.append(1) # last value of the new row
     t.append(nr) # add new row
-
-# output
+# output - whole triangle
 mx = len(" ".join(map(str, t[-1]))) # row max number length
 for r in t: # for each row
     s = " ".join(map(str, r)) # row as string
     print(s.center(mx)) # center aligned
+# output - n'th row
+print(t[n])
 """
   1  
  1 1 
 1 2 1
 """
 """
+how to use of .center()
 text = "안녕"
 
 # 10칸의 중앙에 "안녕"을 넣고, 남은 공간을 공백으로 채움
